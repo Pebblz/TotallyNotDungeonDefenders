a3940bcef0ea881d0a9bc9eb91432e0dba3556a4
Josh Cormier <jcorm714@gmail.com>
2020-12-18 22:17:41 -0500

    Added turret stuff

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 3f;
+    public float timeout = 6f;
+    public Transform target;
+    void Start()
+    {
+        transform.LookAt(target);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+        timeout -= Time.deltaTime;
+        if (timeout<= 0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    public void move()
+    {
+        this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "TARGET")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,15 +7,64 @@ public class Turret : MonoBehaviour
 
 
 {
+    public Queue<GameObject> targets;
+    public float rof = 5;
+    public GameObject bullet;
+    public float projectileSpeed = 4f;
+    private float initRof;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        initRof = rof;
+        targets = new Queue<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(targets.Count);
+        rof -= Time.deltaTime;
+        if (rof <= 0f  )
+        {
+            rof = initRof;
+            if (targets.Peek() != null && targets.Count > 0)
+            {
+                shoot(targets.Peek());
+
+            }
+            
+        }
+    }
+
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+      
+        if (other.gameObject.tag == "TARGET")
+        {
+            this.targets.Enqueue(other.gameObject);
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "TARGET")
+        {
+            //removes exiting target from the queue
+            targets = new Queue<GameObject>(targets.Where(obj => obj != other.gameObject).ToArray());
+        }
+    }
+
+
+    public void shoot(GameObject target)
+    {
+        var b = Instantiate(bullet, this.transform);
+        b.GetComponent<Bullet>().target = target.transform;
+        b.GetComponent<Bullet>().speed =  projectileSpeed;
         
+
     }
 }
