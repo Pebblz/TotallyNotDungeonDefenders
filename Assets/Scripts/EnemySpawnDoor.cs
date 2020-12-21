b6f65d0bbaaa0ddf245bbd08e15a0affc83be2b4
Brandon <Blabrake1@gmail.com>
2020-12-21 14:46:53 -0500

    Spawn door
    Spawn door is almost done i just need to let it know when the wave is over. I got simi spawn weight to work, it'll need to be adjusted when more enemies are added

--- Assets/Scripts/EnemySpawnDoor.cs
+++ Assets/Scripts/EnemySpawnDoor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,16 +7,46 @@ public class EnemySpawnDoor : MonoBehaviour
 {
     GameObject[] EnemyMoveToPoints;
     public GameObject[] CurrentWaveToSpawn;
-    float TimeTillNextSpawn = .1f;
-
+    int segementsOfWavesForLevel;
+    float TimeTillNextSpawn = .2f;
+    //this will be for spawning the enemies in the SpawnEnemy function
+    int SpawnCount = 0;
     void Start()
     {
         CurrentWaveToSpawn = GetComponent<EnemyWave>().enemiesForThisWave;
+        segementsOfWavesForLevel = GetComponent<EnemyWave>().segementsOfWavesForLevel;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (TimeTillNextSpawn < 0 && SpawnCount <= CurrentWaveToSpawn.Length)
+        {
+            SpawnEnemy();
+        }
+        if (TimeTillNextSpawn > -1)
+        {
+            TimeTillNextSpawn -= Time.deltaTime;
+        }
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(CurrentWaveToSpawn[SpawnCount], this.transform.position, Quaternion.identity);
+        SpawnCount++; 
         
+    }
+    bool IsWaveDone()
+    {
+        //this isn't done yet
+
+
+        //for this i'll need to check if all the monsters in the wave have been beaten
+        GetComponent<EnemyWave>().ReadyForNextWave = false;
+        return false;
+    }
+    void SpawnNextWave()
+    {
+        //this as stated in the name will start to spawn the next wave 
+        GetComponent<EnemyWave>().currentWave += 1;
     }
 }
---
+++ Assets/Scripts/EnemyWeight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWeight : MonoBehaviour
+{
+     public EnemyWeights ThisEnemiesWeight;
+
+
+      public enum EnemyWeights {
+        Small,
+        Mid,
+        High,
+        MiniBoss,
+        Boss,
+        FinalBoss
+    };
+}
