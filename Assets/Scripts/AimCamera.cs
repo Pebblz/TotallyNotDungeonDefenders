c6122f3bae668d1ff140cd4f8343ee9fad976a7f
Josh Cormier <jcorm714@gmail.com>
2020-12-28 18:35:55 -0500

    Wooah it's working

---
+++ Assets/Scripts/AimCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCamera : MonoBehaviour
+{
+    public Transform target;
+
+    private Vector3 camOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float smoothFactor = 0.5f;
+
+    public bool lookAtPlayer = false;
+
+    public bool RotateAroundPlayer = true;
+
+    public float RotationSpeed = 5.0f;
+
+    private void Start()
+    {
+        camOffset = transform.position - target.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (RotateAroundPlayer)
+        {
+            Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * RotationSpeed, Vector3.up);
+            camOffset = camTurnAngle * camOffset;
+        }
+
+        Vector3 newPos = target.position + camOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothFactor);
+
+        if (lookAtPlayer || RotateAroundPlayer)
+        {
+            transform.LookAt(target);
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    
+    public float speed = 3f;
+    public float jump = 8f;
+    public bool grounded = true;
+
+    private CharacterController characterController;
+
+    public float RotationSpeed = 240.0f;
+
+    private float Gravity = 20.0f;
+
+    private Vector3 moveDir = Vector3.zero;
+
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = true;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        #region CONTROLS
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+
+        Vector3 camForward_Dir = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+        Vector3 move = v * camForward_Dir + h * Camera.main.transform.right;
+
+        if (move.magnitude > 1f) move.Normalize();
+
+
+        move = transform.InverseTransformDirection(move);
+
+        float turnAmount = Mathf.Atan2(move.x, move.z);
+
+        transform.Rotate(0, turnAmount * RotationSpeed * Time.deltaTime, 0);
+
+        if (characterController.isGrounded)
+        {
+            //jump
+            if (Input.GetKey(KeyCode.Space))
+            {
+                moveDir.y = jump;
+            } else
+            {
+                moveDir = transform.forward * move.magnitude;
+                moveDir *= speed;
+            }
+          
+
+        }
+
+        moveDir.y -= Gravity * Time.deltaTime;
+
+        characterController.Move(moveDir * Time.deltaTime);
+
+       
+        #endregion
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        
+
+    }
+
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+    }
+
+
+  
+
+ 
+}
